1f2adda8b78e99a822bd32f4d3b8ae27c50b5fa8
Anna Nieżychowska <98945924+sto-krotka@users.noreply.github.com>
2022-10-16 11:43:04 +0200

    Update settings.py

--- invoice_proj/settings.py
+++ invoice_proj/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4r-9vzcqp(o=s9t5_$u@cx(4hnwt(4ob8a@svk9argke_u3=&u'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
